fff0110a05421efc2e5f7d51dfad8f650f96fc82
YoshiRulz <OSSYoshiRulz@gmail.com>
2017-12-10 21:21:22 +1100

    Add -i and -f for stdin and file input, respectively

--- jty.sh
+++ jty.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
-# v2017-12-10/00
+# v2017-12-10/01
 
 src=src
 pkg=$src/io/github/yoshirulz/jtysh
 inst=$pkg/JTyshInstantiation.java
 out=out/production/JTysh
-
 mkdir -p $out
-
 cat $pkg/header.java.txt >$inst
-for l in "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"; do
-	if [ "$l" != "" ]; then printf "\t\t%s\n" "$l" >>$inst; fi
-done
-cat $pkg/footer.java.txt >>$inst
 
+case "$1" in
+	"-f")
+		awk '{print"\t\t"$0}' "$2" >>$inst;;
+	"-i")
+		while read l; do
+			if [ -n "$l" ]; then printf "\t\t%s\n" "$l" >>$inst; else break; fi
+		done;;
+	*)
+		for l in "$@"; do
+			if [ -n "$l" ]; then printf "\t\t%s\n" "$l" >>$inst; fi
+		done;;
+esac
+
+cat $pkg/footer.java.txt >>$inst
 javac -sourcepath $src -d $out $pkg/Main.java
 java -classpath $out io.github.yoshirulz.jtysh.Main
