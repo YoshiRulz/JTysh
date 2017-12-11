6d750b4006f9240139afb539ef1f5732334de80b
YoshiRulz <OSSYoshiRulz@gmail.com>
2017-12-11 17:24:08 +1100

    Cleanup, remove `github.clone_(String, CloneOptions)` and its overload

--- src/io/github/yoshirulz/jtysh/JTyshNatLang.java
+++ src/io/github/yoshirulz/jtysh/JTyshNatLang.java
@@ -9,7 +9,6 @@ import io.github.yoshirulz.jtysh.shell.pipecmd.ShellExecMetaWrapperCMD;
 import io.github.yoshirulz.jtysh.uris.GitRepoURI;
 import io.github.yoshirulz.jtysh.uris.GitRepoURI.GitHubRepoURI;
 import io.github.yoshirulz.jtysh.uris.URI;
-import io.github.yoshirulz.jtysh.uris.URIThrowables.InvalidURICastException;
 
 import static io.github.yoshirulz.jtysh.pipeline.pipecmd.TermEchoCMD.ECHO;
 
@@ -19,6 +18,7 @@ import static io.github.yoshirulz.jtysh.pipeline.pipecmd.TermEchoCMD.ECHO;
  * @author YoshiRulz
  * @version 2017-11-28/00
  */
+@SuppressWarnings({"unused", "ClassNamePrefixedWithPackageName"})
 public enum JTyshNatLang { ;
 	/**
 	 * @deprecated The `$.*()` shorthand functions should only be used in the shell, and never in code intended to be run more than once.
@@ -33,45 +33,37 @@ public enum JTyshNatLang { ;
 		public static PipelineVoid echo(Object o) {
 			return Pipeline.from(o.toString()).pipeTo(ECHO);
 		}
+		@SuppressWarnings("StaticMethodNamingConvention")
+		public static void echo$(Object o) { echo(o).r(); }
 		public static ChainablePipeline history() {
 			return History.get();
 		}
-		@SuppressWarnings("StaticMethodNamingConvention")
 		public enum git { ;
+			@SuppressWarnings("StaticMethodNamingConvention")
 			public static ChainablePipeline clone_(GitRepoURI uri, CloneOptions options) {
 				return new NoArgPipelineHead(new GitCloneCMD(uri, options));
 			}
+			@SuppressWarnings("StaticMethodNamingConvention")
 			public static ChainablePipeline clone_(GitRepoURI uri) {
-				return new NoArgPipelineHead(new GitCloneCMD(uri));
+				return clone_(uri, null);
 			}
-			public static ChainablePipeline clone_(String uri, CloneOptions options) throws InvalidURICastException {
+			@SuppressWarnings("StaticMethodNamingConvention")
+			public static ChainablePipeline clone_(String uri, CloneOptions options) {
 				return clone_(GitRepoURI.fromURI(new URI(uri)), options);
 			}
-			public static ChainablePipeline clone_(String uri) throws InvalidURICastException {
-				return clone_(GitRepoURI.fromURI(new URI(uri)));
+			@SuppressWarnings("StaticMethodNamingConvention")
+			public static ChainablePipeline clone_(String uri) {
+				return clone_(uri, null);
 			}
 		}
-		@SuppressWarnings("StaticMethodNamingConvention")
 		public enum github { ;
+			@SuppressWarnings("StaticMethodNamingConvention")
 			public static ChainablePipeline clone_(String user, String repo, CloneOptions options) {
 				return git.clone_(new GitHubRepoURI(user, repo), options);
 			}
+			@SuppressWarnings("StaticMethodNamingConvention")
 			public static ChainablePipeline clone_(String user, String repo) {
-				return git.clone_(new GitHubRepoURI(user, repo));
-			}
-			/**
-			 * @param s Of the form "user/repo"
-			 */
-			public static ChainablePipeline clone_(String s, CloneOptions options) {
-				String[] a = s.split("/");
-				return clone_(a[0], a[1], options);
-			}
-			/**
-			 * @param s Of the form "user/repo"
-			 */
-			public static ChainablePipeline clone_(String s) {
-				String[] a = s.split("/");
-				return clone_(a[0], a[1]);
+				return clone_(user, repo, null);
 			}
 		}
 	}
